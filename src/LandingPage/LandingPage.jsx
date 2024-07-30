2774f8f407ed2179cfc008c44d640cd0c1d7d2de
au-lex <aulexc4d2@gmail.com>
2024-07-30 21:50:44 +0100

    hero text

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Componet/Header'
+import Hero from '../Componet/Hero'
 
 const LandingPage = () => {
   return (
     <>
     
     <Header />
+    <Hero />
     
     </>
   )
